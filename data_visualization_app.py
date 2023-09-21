f1081c786416b4abd12d11349e3eaf5d6db5acaf
taufiksatrian <72427297+taufiksatrian@users.noreply.github.com>
2023-09-22 03:53:44 +0700

    Update visualization with Tableau

--- data_visualization_app.py
+++ data_visualization_app.py
@@ -3,7 +3,9 @@ import pandas as pd
 import seaborn as sns
 import streamlit as st
 import matplotlib.pyplot as plt
+import tableauserverclient as TSC
 from matplotlib.patches import Patch
+import streamlit.components.v1 as components
 
 data_training = pd.read_csv('data_sintetis_akhir.csv', sep=';')
 
@@ -68,3 +70,64 @@ plt.text(0.19, -0.12, keterangan, ha='center', va='center',
 
 
 st.pyplot(plt)
+
+
+tableau_html = """
+<div class="centered" style="text-align: center;">
+    <div class='tableauPlaceholder' id='viz1695328600249' style='display:inline-block;'>
+        <noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;TracerStudyUnsoed1-2&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript>
+        <object class='tableauViz' style='display:none; width: 100%;'>
+            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+            <param name='embed_code_version' value='3' />
+            <param name='site_root' value='' />
+            <param name='name' value='TracerStudyUnsoed1-2&#47;Dashboard1' />
+            <param name='tabs' value='no' />
+            <param name='toolbar' value='yes' />
+            <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;TracerStudyUnsoed1-2&#47;Dashboard1&#47;1.png' />
+            <param name='animate_transition' value='yes' />
+            <param name='display_static_image' value='yes' />
+            <param name='display_spinner' value='yes' />
+            <param name='display_overlay' value='yes' />
+            <param name='display_count' value='yes' />
+            <param name='language' value='en-US' />
+        </object>
+    </div>
+</div>
+<script type='text/javascript'>
+    var divElement = document.getElementById('viz1695328600249');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if ( divElement.offsetWidth > 1200 ) {
+        vizElement.style.width='1200px';
+        vizElement.style.height='827px';
+    } else if ( divElement.offsetWidth > 800 ) {
+        vizElement.style.width='1000px';
+        vizElement.style.height='827px';
+    } else {
+        vizElement.style.width='100%';
+        vizElement.style.height='1277px';
+    }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+</script>
+"""
+
+st.markdown("""
+<style>
+.centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.write(
+    """
+    ### Dashboard 
+Dashboard menggunakan Tools Tableau:
+"""
+)
+
+components.html(tableau_html, height=1000, width=1200)
